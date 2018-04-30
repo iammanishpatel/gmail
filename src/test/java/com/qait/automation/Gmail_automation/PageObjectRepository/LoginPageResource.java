61846a24db799235747a9d05d5a161ebd0114a41
Manish Patel <manishpatel@QAIT.COM>
2018-04-30 16:20:01 +0530

    enter your password in LoginPageResource.java

--- src/test/java/com/qait/automation/Gmail_automation/PageObjectRepository/LoginPageResource.java
+++ src/test/java/com/qait/automation/Gmail_automation/PageObjectRepository/LoginPageResource.java
@@ -6,7 +6,7 @@ public class LoginPageResource {
 	public static final String usernamePath = "//input[@id='identifierId']";
 	public static final String passwordPath = "//input[@autocomplete='current-password']";
 	public static final String username = "manishtsk007";
-	public static final String password = "Qainfotech1212#";
+	public static final String password = "enter your password";  // enter password here...
 	public static final String next = "//*[text()='Next']";
 	public static final String googleAppIcon = "//a[@href='https://www.google.co.in/intl/en/options/']";
 	public static final String gmailIcon = "//a[@href='https://mail.google.com/mail/']";
